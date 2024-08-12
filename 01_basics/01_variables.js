70616ebb6d4e27890d58ad05734d43964e36f0b2
omujjwal121 <131457579+omujjwal121@users.noreply.github.com>
2024-08-12 19:48:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "avc@gmail.com"
+var pass = "235698"
+city = "Jaipur"
+let state
+// accountId = 526478
+
+console.table([accountId, accountEmail, pass, city, state])
